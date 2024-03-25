0bad48187e0491afd2d7f533934fffc63d4b7ae5
Alan <alanitoyah96@gmail.com>
2024-03-24 21:15:24 -0500

    Implemented getDataPoint function to ensure the accurate retrieval of stock information including stock name, bid price, ask price, and price (calculated as the average of bid and ask prices). Updated getRatio function to correctly calculate the ratio between two stock prices. Refined main function to output precise stock information, bid and ask prices, and the corresponding price ratio.  Created different test case to guarantee edits were properly made

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
+
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -23,3 +24,29 @@ class ClientTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+
+class TestYourFuction(unittest.TestCase):
+  def test_divide_by_zero(self):
+    # Test of when price_b is 0
+    price_a = 5
+    price_b = 0
+    self.assertIsNone(getRatio(price_a, price_b))
+
+  def test_a_is_zero(self):
+    # Test of when price_a is 0
+    price_a = 0
+    price_b = 10
+    self.assertEqual(getRatio(price_a, price_b), 0)
+
+  def normal_case(self):
+    # When neither inputs are 0 and both are integers
+    price_a = 300
+    price_b = 30
+    self.assertEqual(getRatio(price_a, price_b), 10)
+
+  def float_variable_a(self):
+    # When neither inputs are 0 and price_a is a float
+    price_a = 12.5
+    price_b = 10
+    self.assertEqual(getRatio(price_a, price_b), 1.25)
